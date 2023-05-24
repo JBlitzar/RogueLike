9f3a59e2d0517a0d3bc674341b70f20bb3380ab1
JBlitzar <90664831+JBlitzar@users.noreply.github.com>
2023-05-24 16:20:20 -0700

    enemy skeleton

---
+++ Enemy.gd
@@ -0,0 +1,10 @@
+extends CharacterBody2D
+var speed = 16
+var target: Vector2 = position
+func _ready():
+	pass
+func _process(delta):
+	velocity = position.direction_to(target) * speed
+	move_and_slide()
+func advance_next(localX,localY):
+	target = Vector2(localX,localY)
